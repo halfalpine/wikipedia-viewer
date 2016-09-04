d5cac490b015c66e377ff642ef51c3037cff9b26
halfalpine <rocksandwoks@gmail.com>
2016-09-03 23:22:50 -0400

    Latest updates from Mac Mini

--- js/main.js
+++ js/main.js
@@ -14,51 +14,23 @@ $(document).ready(function() {
 
   function getSearch() {
     var searchFor = $("#searchbar").val();
-    $.ajax({
-      url: 'https://en.wikipedia.org/w/api.php',
-      data: {
-        action: 'query',
-        meta: 'tokens',
-        format: 'json',
-        origin: 'https://www.mediawiki.org'
-      },
-      xhrFields: {
-        withCredentials: true
-      },
-      dataType: 'json'
-    }).done(function(data) {
-      $.ajax({
-        url: 'https://en.wikipedia.org/w/api.php?origin=https://www.mediawiki.org',
-        method: 'POST',
-        data: {
-          action: 'options',
-          format: 'json',
-          token: data.query.tokens.csrftoken,
-          optionname: 'userjs-test',
-          optionvalue: 'Hello world!'
-        },
-        xhrFields: {
-          withCredentials: true
-        },
-        dataType: 'json'
-      });
-    });
     $.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&exintro&generator=search&gsrsearch=" + searchFor + "&callback=?", showResults);
   }
 
   function showResults(data) {
+    console.log(data)
     var raw_json = data;
-    var pages;
     $("#results-list").empty();
     if (raw_json && raw_json.query && raw_json.query.pages) {
-      pages = raw_json.query.pages;
+      var pages = raw_json.query.pages;
     } else {
       $("#results").html("Search error!");
     }
     for (var id in pages) {
       if (pages[id].title && pages[id].extract) {
-        $("#results-list").append("<li><ul class='search-result-item'><li><a href='http://en.wikipedia.org/?curid=" + pages[id].pageid + "' target='_blank'>" + pages[id].title + "</a></li><li class='extract'>" + pages[id].extract + "</li></ul>");
-      } else {
+        $("#results-list").append("<li><ul class='search-result-item'><li><a href='http://en.wikipedia.org/?curid=" + pages[id].pageid + "' target='_blank'>" + pages[id].title + "</a></li><li class='extract'>" + pages[id].extract) + "</li></ul>";
+      }
+      else {
         $("#results").html("Search error!");
       }
     }
